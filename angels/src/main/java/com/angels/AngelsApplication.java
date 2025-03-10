6144cc8a8e10d80ef419d2c1ee2756bd41071776
eunjuni1221 <kokomo1221@naver.com>
2025-03-10 11:00:33 +0900

    first

---
+++ angels/src/main/java/com/angels/AngelsApplication.java
@@ -0,0 +1,13 @@
+package com.angels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelsApplication.class, args);
+	}
+
+}
